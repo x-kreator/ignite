62b1d9c4fca2b567262f1dd4494dcc74f77c3bb3
Dmitriy Sorokin <sbt.sorokin.dvl@gmail.com>
2018-06-29 17:18:23 +0300

    IGNITE-8719 Index left partially built if a node crashes during index create or rebuild

--- modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/index/DynamicIndexAbstractConcurrentSelfTest.java
+++ modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/index/DynamicIndexAbstractConcurrentSelfTest.java
@@ -25,8 +25,10 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
 import javax.cache.Cache;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
@@ -39,16 +41,22 @@ import org.apache.ignite.cache.CacheMode;
 import org.apache.ignite.cache.QueryIndex;
 import org.apache.ignite.cache.query.SqlQuery;
 import org.apache.ignite.configuration.CacheConfiguration;
+import org.apache.ignite.configuration.DataStorageConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.internal.IgniteClientReconnectAbstractTest;
 import org.apache.ignite.internal.IgniteEx;
 import org.apache.ignite.internal.IgniteInternalFuture;
+import org.apache.ignite.internal.pagemem.PageIdAllocator;
+import org.apache.ignite.internal.processors.cache.persistence.file.FilePageStore;
+import org.apache.ignite.internal.processors.cache.persistence.file.FilePageStoreManager;
 import org.apache.ignite.internal.processors.query.GridQueryProcessor;
 import org.apache.ignite.internal.processors.query.QueryIndexDescriptorImpl;
 import org.apache.ignite.internal.processors.query.h2.IgniteH2Indexing;
 import org.apache.ignite.internal.processors.query.schema.SchemaIndexCacheVisitor;
 import org.apache.ignite.internal.processors.query.schema.SchemaOperationException;
+import org.apache.ignite.internal.util.typedef.G;
 import org.apache.ignite.internal.util.typedef.T3;
+import org.apache.ignite.internal.util.typedef.internal.CU;
 import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
 import org.jetbrains.annotations.NotNull;
 
@@ -103,12 +111,9 @@ public abstract class DynamicIndexAbstractConcurrentSelfTest extends DynamicInde
 
         stopAllGrids();
 
-        super.afterTest();
-    }
+        cleanPersistenceDir();
 
-    /** {@inheritDoc} */
-    @Override protected long getTestTimeout() {
-        return 5 * 60 * 1000L;
+        super.afterTest();
     }
 
     /** {@inheritDoc} */
@@ -129,6 +134,23 @@ public abstract class DynamicIndexAbstractConcurrentSelfTest extends DynamicInde
             testSpi.setIpFinder(((TcpDiscoverySpi)cfg.getDiscoverySpi()).getIpFinder());
 
         return cfg.setDiscoverySpi(testSpi);
+    }
+
+    /**
+     * @param idx Index.
+     */
+    protected IgniteConfiguration pdsConfiguration(int idx) throws Exception {
+        IgniteConfiguration cfg = serverConfiguration(idx);
+
+        DataStorageConfiguration dsCfg = cfg.getDataStorageConfiguration()
+            .setPageSize(2048)
+            .setWalSegmentSize(512 * 1024);
+
+        dsCfg.getDefaultDataRegionConfiguration()
+            .setPersistenceEnabled(true)
+            .setMaxSize(2048L * 1024 * 1024);
+
+        return cfg;
     }
 
     /**
@@ -472,6 +494,230 @@ public abstract class DynamicIndexAbstractConcurrentSelfTest extends DynamicInde
         catch (SchemaOperationException e) {
             // No-op.
         }
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testIndexPartialBuild() throws Exception {
+        checkIndexPartialBuild(1);
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testIndexPartialBuild_2Nodes() throws Exception {
+        checkIndexPartialBuild(2);
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testIndexPartialBuild_4Nodes() throws Exception {
+        checkIndexPartialBuild(4);
+    }
+
+    /**
+     * @param nodeCnt Node count.
+     */
+    private void checkIndexPartialBuild(final int nodeCnt) throws Exception {
+        cleanPersistenceDir();
+
+        final IgniteEx node = startPdsGrids(nodeCnt);
+
+        node.cluster().active(true);
+
+        node.cluster().setBaselineTopology(node.cluster().currentBaselineTopology());
+
+        createSqlCache(node);
+
+        final int cacheSize = LARGE_CACHE_SIZE;
+
+        put(node, 0, cacheSize);
+
+        forceCheckpointParallel();
+
+        // Start index operation in blocked state.
+        CountDownLatch idxLatch1 = blockIndexing(node);
+
+        QueryIndex idx = index(IDX_NAME_1, field(FIELD_NAME_1));
+
+        final IgniteInternalFuture<?> idxFut =
+            queryProcessor(node).dynamicIndexCreate(CACHE_NAME, CACHE_NAME, TBL_NAME, idx, false, 0);
+
+        idxLatch1.await();
+
+        AtomicBoolean idxDone = new AtomicBoolean();
+
+        final CountDownLatch idxPagesChanged = new CountDownLatch(1);
+
+        new Thread() {
+            public void run() {
+                int pages = totalIndexPages();
+
+                System.out.println(">>> index pages: " + pages);
+
+                while (!idxDone.get()) {
+                    if (totalIndexPages() >= pages + cacheSize / 1000) {
+                        System.out.println(">>> index pages changed: " + totalIndexPages());
+
+                        idxPagesChanged.countDown();
+
+                        break;
+                    }
+
+                    Thread.yield();
+                }
+            }
+        }.start();
+
+        unblockIndexing(node);
+
+        idxPagesChanged.await(2000, TimeUnit.MILLISECONDS);
+
+        System.out.println(">>> index pages before checkpoint: " + totalIndexPages());
+
+        forceCheckpointParallel();
+
+        System.out.println(">>> index pages after checkpoint: " + totalIndexPages());
+
+        stopAllGridsParallel();
+
+        boolean idxFutComplete = false;
+
+        try {
+            idxFut.get(30_000);
+
+            idxFutComplete = true;
+
+            log.warning("Exception has not been thrown on idxFut.get()!");
+        }
+        catch (SchemaOperationException e) {
+            e.printStackTrace();
+        }
+
+        idxDone.set(true);
+
+        System.out.println(">>> index pages after stop: " + totalIndexPages());
+
+        final IgniteEx nodeRestarted = startPdsGrids(nodeCnt);
+
+        assertTrue(nodeRestarted.cluster().active());
+
+        System.out.println(">>> index pages after restart: " + totalIndexPages());
+
+        if (idxFutComplete)
+            assertIndexUsed(IDX_NAME_1, SQL_SIMPLE_FIELD_1, SQL_ARG_1);
+        else
+            assertIndexNotUsed(IDX_NAME_1, SQL_SIMPLE_FIELD_1, SQL_ARG_1);
+
+        assertSqlSimpleData(SQL_SIMPLE_FIELD_1, cacheSize - SQL_ARG_1);
+    }
+
+    /**
+     * @param cnt Count.
+     */
+    private IgniteEx startPdsGrids(int cnt) throws Exception {
+        assertTrue(cnt > 0);
+
+        final IgniteEx srv1 = (IgniteEx)ignitionStart(pdsConfiguration(1));
+
+        for (int i = 2; i <= cnt; i++)
+            ignitionStart(pdsConfiguration(i));
+
+        return srv1;
+    }
+
+    /**
+     *
+     */
+    private void stopAllGridsParallel() throws InterruptedException {
+        forAllGridsDoParallel("stop", g -> stopGrid(g.name(), true));
+    }
+
+    /**
+     *
+     */
+    private void forceCheckpointParallel() throws InterruptedException {
+        forAllGridsDoParallel("cp", g -> {
+            try {
+                forceCheckpoint(g);
+            }
+            catch (IgniteCheckedException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    /**
+     * @param threadSuffix Thread suffix.
+     * @param consumer Consumer.
+     */
+    private void forAllGridsDoParallel(final String threadSuffix, final Consumer<Ignite> consumer) throws InterruptedException {
+        List<Ignite> grids = G.allGrids();
+
+        if (grids.isEmpty())
+            return;
+
+        if (grids.size() == 1) {
+            consumer.accept(grids.iterator().next());
+
+            return;
+        }
+
+        CountDownLatch latch = new CountDownLatch(grids.size());
+
+        for (final Ignite g : grids) {
+            new Thread(g.name() + "-" + threadSuffix) {
+                public void run() {
+                    try {
+                        consumer.accept(g);
+                    }
+                    finally {
+                        latch.countDown();
+                    }
+                }
+            }.start();
+        }
+
+        latch.await();
+    }
+
+    /**
+     * @param node Node.
+     * @param partId Partition id.
+     * @return Page store of {@code node} with specified partition id.
+     */
+    protected FilePageStore getPageStore(Ignite node, int partId) {
+        FilePageStoreManager pageStoreMgr = (FilePageStoreManager)((IgniteEx)node).context().cache().context().pageStore();
+
+        assert pageStoreMgr != null : "Persistence is not enabled";
+
+        try {
+            return (FilePageStore)pageStoreMgr.getStore(CU.cacheId(CACHE_NAME), partId);
+        }
+        catch (IgniteCheckedException e) {
+            throw new IgniteException(e);
+        }
+    }
+
+    /**
+     * @param node Node.
+     * @return Index page store of {@code node}.
+     */
+    protected FilePageStore getIndexPageStore(Ignite node) {
+        return getPageStore(node, PageIdAllocator.INDEX_PARTITION);
+    }
+
+    /**
+     * @return Total index pages count.
+     */
+    protected int totalIndexPages() {
+        return Ignition.allGrids().stream()
+            .map(this::getIndexPageStore)
+            .map(FilePageStore::pages)
+            .reduce(Integer::sum)
+            .orElse(0);
     }
 
     /**
